1a468ceecd0bba9e0927752e81ad902ef127d7c6
Amgero <amgero@outlook.com>
2023-08-30 00:08:11 -0300

    new files

---
+++ dice-roller.js
@@ -0,0 +1,4 @@
+//write a program that takes a single parameter from command line, a number, and rolls that many six-sided dice
+// node dice-rolle.js 3 --> Rolled 3 dice: output 2, 6, 5
+//Math.random
+
